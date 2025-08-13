c5afab6454bd2b0a2f3f632ef5027dac95f4e397
joey <joe.burge.iii@gmail.com>
2025-08-13 07:00:02 -0700

    updated sg to grep buffer, and sfg to grep file system

--- init.lua
+++ init.lua
@@ -332,9 +332,11 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sfg', builtin.live_grep, { desc = '[S]earch [F]iles by [G]rep' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sg', builtin.current_buffer_fuzzy_find, { desc = '[S]earch by [G]rep' })
+
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -554,7 +556,7 @@ require('lazy').setup({
         },
 
         -- no format on save for this guy :), but need the lsp, just toggle the warnings with <leader>dt
-        pylsp = { pylsp = {} },
+        basedpyright = { basedpyright = {} },
         lua_ls = {
           settings = {
             Lua = { completion = { callSnippet = 'Replace' } },
